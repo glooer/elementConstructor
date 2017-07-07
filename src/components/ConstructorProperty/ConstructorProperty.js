06dbc27794282fba46c7cee4734d035f71cb9b38
Kostya Polenkov <kostya14@gmail.com>
2017-07-07 17:33:59 +0500

    электр

--- src/components/ConstructorProperty/ConstructorProperty.js
+++ src/components/ConstructorProperty/ConstructorProperty.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
 export default class ConstructorProperty extends Component {
-	render() {
+
+	constructor() {
+		super();
+
+		this.state = {
+			currentElementId: 123,
+			currentElementProps: {
+				'text': '123',
+				'col': 'reqw',
+				'src': 'https://placehold.it/200x200'
+			}
+		}
+	}
+
+	renderInputGroup(key, value) {
 		return (
-			null
+			<div className="form-group">
+				<label>{ key }</label>
+				<input type="text" className="form-control" defaultValue={ value } />
+			</div>
+		)
+	}
+
+	render() {
+		let props = this.state.currentElementProps
+
+		return (
+			<div className="col-lg-2">
+				<h3>Текущий номер элемента: { this.state.currentElementId }</h3>
+				<div>
+					{ Object.keys(props).map(key => {
+							let value = props[key]
+							return this.renderInputGroup(key, value);
+						})
+					}
+				</div>
+				<div className="form-group">
+					<button className="btn btn-info">Сохранить</button>
+				</div>
+			</div>
 		)
 	}
 }
